9faa142b6dc4b36264a14f80bf6856c7e2aca08c
eeapbh <eeapbh@gmail.com>
2021-01-13 23:34:18 +0900

    0113

--- JUNGOL/1997.떡먹는호랑이.py
+++ JUNGOL/1997.떡먹는호랑이.py
@@ -1,1 +1,20 @@
 d, k = map(int, input().split())
+arr = [0]*31
+def f(x):
+    arr[0] = 1
+    arr[1] = 1
+    arr[2] = 2
+    for i in range(3, x+1):
+        arr[i] = arr[i-2] + arr[i-1]
+    return arr[x]
+
+def check():
+    for i in range(1,10000):
+        for j in range(1, i):
+            if f(d - 3) * j + f(d - 2) * i == k:
+                return j, i
+
+a, b = check()
+print(a)
+print(b)
+
--- JUNGOL/연습.py
+++ JUNGOL/연습.py
@@ -1,3 +1,13 @@
-a = {}
-a['a'] = 1
-print(a)
+arr = [0]*31
+
+def f(x):
+    arr[0] = 1
+    arr[1] = 1
+    arr[2] = 2
+    for i in range(3, x+1):
+        arr[i] = arr[i-2] + arr[i-1]
+    return arr[x]
+
+
+
+print(f(5))

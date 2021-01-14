4189b5b2f7945a57b3b6c2d178f9b164f9bb111c
eeapbh <eeapbh@gmail.com>
2021-01-14 21:02:28 +0900

    0114

--- JUNGOL/1671.색종이(중).py
+++ JUNGOL/1671.색종이(중).py
@@ -1,10 +1,30 @@
 import sys
 sys.stdin = open('input.txt')
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# 젤 작은 x와 젤 긴 y를 찾는다 그테두리를 1로 채워넣는다
-#
+
+def dfs(x, y):
+    global ans
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < 110 and 0 <= ny < 110:
+            if arr[nx][ny] == 0:
+                ans += 1
+
+
+arr = [[0] * 110 for _ in range(110)]
 n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
+    for i in range(y, y + 10):
+        for j in range(x, x + 10):
+            arr[i][j] = 1
 
-
-
+ans = 0
+for i in range(110):
+    for j in range(110):
+        if arr[i][j] == 1:
+            dfs(i, j)
 print(ans)

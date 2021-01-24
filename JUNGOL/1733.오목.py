b484e5c9b3f5ab0d179eae4c87232ded100e33a6
eeapbh <eeapbh@gmail.com>
2021-01-24 17:43:58 +0900

    0124

--- JUNGOL/1733.오목.py
+++ JUNGOL/1733.오목.py
@@ -7,13 +7,17 @@ dx = [1, 1, 0, -1]
 dy = [0, 1, 1, 1]
 def check(num, x, y, cnt, idx):
     if cnt == 5:
-        return 1
+        if arr[x+dx[idx]][y+dy[idx]] != num and arr[x-5*dx[idx]][y-5*dy[idx]] != num:
+            return 1
+
     for i in range(4):
+        if cnt > 1 and idx != i:
+            continue
         nx = x+dx[i]
         ny = y+dy[i]
         if 0<=nx<19 and 0<=ny<19:
             if arr[nx][ny] == num:
-                return check(num, nx, ny, cnt+1, idx)
+                return check(num, nx, ny, cnt+1, i)
 
 
 def ans():
@@ -23,6 +27,10 @@ def ans():
                 rs = check(arr[j][i], j, i, 1, 0)
                 if rs == 1:
                     return arr[j][i], j+1, i+1
+    return 0, 0, 0
 num, x, y = ans()
-print(num)
-print(x, y)
+if num == 0:
+    print(num)
+else:
+    print(num)
+    print(x, y)

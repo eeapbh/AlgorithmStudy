c09c9b06c4ddc1c27cc69fd2b3a99003cf1b81a5
박봉현 <eeapbh@gmail.com>
2021-03-27 23:56:32 +0900

    0327

---
+++ baekjoon/1003.피보나치함수.py
@@ -0,0 +1,17 @@
+def check(n):
+    global zero, one
+    if n == 0:
+        zero += 1
+    elif n == 1:
+        one += 1
+    else:
+
+
+
+zero = 0
+one = 0
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+

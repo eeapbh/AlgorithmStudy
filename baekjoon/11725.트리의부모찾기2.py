6d7d37659844e34361c19c44dc4c2121b4af1d93
박봉현 <eeapbh@gmail.com>
2021-04-20 08:58:49 +0900

    0420

---
+++ baekjoon/11725.트리의부모찾기2.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n = int(input())
+tree = [[] for _ in range(n + 1)]
+for i in range(n-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+q = deque()
+q.append(1)
+ans = {}
+check = [0 for i in range(n+1)]
+while len(q) > 0:
+    parent = q.popleft()
+    for i in tree[parent]:
+        if not check[i]:
+            ans[i] = parent
+            q.append(i)
+            check[i] = 1
+
+for i in range(2, n+1):
+    print(ans[i])
+

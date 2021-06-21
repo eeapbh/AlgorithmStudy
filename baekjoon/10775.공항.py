7a6f8fbcd03863e4400c9c5a72c3f113fc69484b
eeapbh <eeapbh@gmail.com>
2021-06-21 17:40:02 +0900

    0621

--- baekjoon/10775.공항.py
+++ baekjoon/10775.공항.py
@@ -3,15 +3,28 @@ sys.stdin = open('input.txt', 'r')
 g = int(input())
 p = int(input())
 arr = [int(input()) for _ in range(p)]
-gate = [0]*(g+1)
+gate = [i for i in range(g + 1)]
+
+def union(x, y):
+
+    a = find(x)
+    b = find(y)
+    gate[a] = b
+
+
+def find(x):
+    if gate[x] == x:
+        return x
+    gate[x] = find(gate[x])
+    return gate[x]
+
 cnt = 0
-for a in arr:
-    for i in range(1, a+1):
-        if gate[i] == 0:
-            gate[i] = 1
-            cnt += 1
-            break
-    else:
+for i in arr:
+    p = find(i)
+    if p == 0:
         break
+    else:
+        union(p, p-1)
+        cnt += 1
 
 print(cnt)

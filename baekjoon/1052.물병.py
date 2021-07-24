1ebdc2685121719098c6a89d3c8d8631faca5149
eeapbh <eeapbh@gmail.com>
2021-07-24 16:17:14 +0900

    0724

--- baekjoon/1052.물병.py
+++ baekjoon/1052.물병.py
@@ -1,12 +1,4 @@
 n, k = map(int, input().split())
 arr = [1]*n
 while 1:
-    arr.sort(reverse=True)
-    tmp = []
-    if arr.count(0) <= k:
-        break
-    for i in range(n-2):
-        if arr[i] == arr[i + 1]:
-            tmp.append(arr[i] + arr[i + 1])
-            arr[i + 1] = 0
-
+   pass
